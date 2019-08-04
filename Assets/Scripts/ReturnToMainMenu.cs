0aca0a9dfab559cfd147982ff8aa4e37a8bad02d
coleandress <36573523+coleandress@users.noreply.github.com>
2019-08-04 10:26:05 -0500

    Added a way to return to main menu from win and lose screens.

---
+++ Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
